97e64c74128caf77ce88383478ca808e6485d23b
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-07-06 09:57:35 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto">
+      <div className="card">
+        <div className="card-header">Hello</div>
+        <div className="card-body">
+          <p className="p-2">Hello</p>
+
+        </div>
+
+      </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
